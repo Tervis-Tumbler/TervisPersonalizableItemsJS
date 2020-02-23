726694c9ef01fc91e7c0f2f7b9ba0be7d6ab2878
Hamilton Perez <hambone124@users.noreply.github.com>
2020-02-22 23:14:15 -0500

    Handle undefined $ItemNumber

--- TervisPersonalizableItems.js
+++ TervisPersonalizableItems.js
@@ -5,18 +5,27 @@ let $PersonalizableItems
 export async function Test_IsTervisItemPersonalizable({
     $ItemNumber
 }) {
+    /*
     //duplicated to work around top level await, remove once top level await is supported
     if (!$IsBrowser) {
         var fetch = (await import("node-fetch")).default
     } else {
         var fetch = window.fetch
     }
-
+    */
+    
     if (!$PersonalizableItems) {
         $PersonalizableItems = await fetch(
             `https://unpkg.com/@tervis/tervispersonalizableitemsjs/TervisPersonalizableItems.json`
         ).then($Response => $Response.json())
     }
 
-    return $PersonalizableItems.includes($ItemNumber.toString())
+    let $IsPersonalizable 
+    if ($ItemNumber !== undefined) {
+        $IsPersonalizable = $PersonalizableItems.includes($ItemNumber.toString())
+    } else {
+        $IsPersonalizable = false
+    }
+    
+    return $IsPersonalizable
 }
